ec2a1afea7390a34ff4271d26833a7b923d3f37a
Jonathan Mai <jonm@live.fr>
2024-03-21 21:59:35 +0100

    Page About

---
+++ src/components/Collapse/Collapse.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import "./Collapse.scss";
+
+const angleDown = <FontAwesomeIcon icon={faChevronDown} />;
+const angleUp = <FontAwesomeIcon icon={faChevronUp} />;
+
+function Collapse(props) {
+  const [open, setOpen] = useState(false);
+
+  function toggle() {
+    setOpen(!open);
+  }
+ 
+  return (
+    <>
+     
+        <div onClick={toggle} className="collapse">
+          <h2 className="collapse_title">{props.label}</h2>
+          {!open ? (
+            <i className="collapse_icon">{angleDown}</i>
+          ) : (
+            <i className="collapse_icon">{angleUp}</i>
+          )}
+        </div>
+        {open && <div className="collapse_content">{props.content}</div>}
+      
+    </>
+  );
+}
+
+export default Collapse;
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,42 @@
 import Banner from "../../components/Banner/Banner";
-import BannerImg from '../../assets/bannerabout.png'
-
+import BannerImg from "../../assets/bannerabout.png";
+import Collapse from "../../components/Collapse/Collapse";
 
 function About() {
-    return (
-        <Banner imgUrl={ BannerImg }/>
-    )
+  return (
+    <>
+      <Banner imgUrl={BannerImg} />
+      <article className="collapse_container" style={{ marginTop: "40px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <Collapse
+          label="Fiabilité"
+          content="Les annonces postées sur Kasa garantissent une fiabilité totale. 
+                    Les photos sont conformes aux logements, et toutes les informations 
+                    sont régulièrement vérifiées par nos équipes."
+        />
+        <Collapse
+          label="Respect"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. 
+                    Tout comportement discriminatoire ou de perturbation du voisinage
+                    entraînera une exclusion de note plateforme."
+        />
+        <Collapse
+          label="Service"
+          content="Nos équipes se tiennent à votre disposition pour vous fournir une 
+                    expérience parfaite. N'hésitez pas à nous contacter si vous avez 
+                    la moindre question."
+        />
+        <Collapse
+          label="Sécurité"
+          content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que 
+                    pour les voyageurs, chaque logement correspond aux critères de sécurité
+                    établis par nos services. En laissant une note aussi bien à l'hôte qu'au
+                    locataire, cela permet à nos équipes de vérifier que les standards sont 
+                    bien respectés. Nous organisons également des ateliers sur la sécurité 
+                    domestique pour nos hôtes."
+        />
+      </article>
+    </>
+  );
 }
 
-export default About
+export default About;
